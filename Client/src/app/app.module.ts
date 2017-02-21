e376772586f280ca52e243bbf0cff0be95580f84
michaeldeongreen <michael.green@architectinginnovation.com>
2017-02-20 19:00:43 -0600

    *Created base components for the Angular 2 Application

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SideWidgetComponent } from './side-widget/side-widget.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent,
+    CategoriesComponent,
+    SideWidgetComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
