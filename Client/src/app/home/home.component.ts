b26a278d022c330bf356a3c6d60ce33675406252
michaeldeongreen <michael.green@architectinginnovation.com>
2017-02-21 20:15:18 -0600

    *Got server side paging simulation working

--- Client/src/app/home/home.component.ts
+++ Client/src/app/home/home.component.ts
@@ -1,19 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
+
+interface IServerResponse {
+    items: string[];
+    total: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
-    collection = [];
+    meals: string[] = [];
+    asyncMeals: Observable<string[]>;
+    p: number = 1;
+    total: number;
+    loading: boolean;
+
     constructor() {
         for (let i = 1; i <= 100; i++) {
-            this.collection.push(`item ${i}`);
+            this.meals.push(`Meal ${i}`);
         }
     }
 
-  ngOnInit() {
+    ngOnInit() {
+        this.getPage(1);
   }
 
+  getPage(page: number) {
+      this.loading = true;
+      this.asyncMeals = serverCall(this.meals, page)
+          .do(res => {
+              this.total = res.total;
+              this.p = page;
+              this.loading = false;
+          })
+          .map(res => res.items);
+  }
+}
+
+/**
+ * Simulate an async HTTP call with a delayed observable.
+ */
+function serverCall(meals: string[], page: number): Observable<IServerResponse> {
+    const perPage = 5;
+    const start = (page - 1) * perPage;
+    const end = start + perPage;
+
+    return Observable
+        .of({
+            items: meals.slice(start, end),
+            total: 100
+        }).delay(1000);
 }
