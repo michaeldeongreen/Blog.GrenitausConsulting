4d82e880f20da2bb22b35d53b621e5720e9719bd
michaeldeongreen <michaeldeongreen@gmail.com>
2017-04-21 21:36:59 -0500

    *Upgraded typescript

--- Client/src/environments/environment.js
+++ Client/src/environments/environment.js
@@ -2,6 +2,7 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
 export var environment = {
     production: false,
     apiUrl: 'http://localhost:49529/api'
