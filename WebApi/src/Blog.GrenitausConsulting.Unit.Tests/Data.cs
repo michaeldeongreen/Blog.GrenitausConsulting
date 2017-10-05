41f7e905c4c75dabb8f0df0a658da7b3a4115fa0
michaeldeongreen <michaeldeongreen@gmail.com>
2017-10-04 20:43:54 -0500

    *Fixed broken unit tests, only found out because I am fooling around with TeamCity/Octopus Deploy

--- WebApi/src/Blog.GrenitausConsulting.Unit.Tests/Data.cs
+++ WebApi/src/Blog.GrenitausConsulting.Unit.Tests/Data.cs
@@ -14,7 +14,7 @@ namespace Blog.GrenitausConsulting.Unit.Tests
             List<PostSummary> posts = new List<PostSummary>();
             for (int i = 1; i <= 100; i++)
             {
-                posts.Add(new PostSummary() { Id = i });
+                posts.Add(new PostSummary() { Id = i, IsActive = true });
             }
             return posts;
         }
