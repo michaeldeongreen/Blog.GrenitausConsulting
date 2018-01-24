a053b3317086d71cce9380caaffb76f110976249
michaeldeongreen <michaeldeongreen@gmail.com>
2018-01-23 20:06:24 -0600

    *Added CategoryController, will stop now because I want to research some of the new filters and why json is auot configured

---
+++ Core.WebApi/src/Blog.GrenitausConsulting.Core.Web.Api/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Blog.GrenitausConsulting.Core.Domain;
+using Blog.GrenitausConsulting.Core.Common;
+
+namespace Blog.GrenitausConsulting.Core.Web.Api.Controllers
+{
+    [Produces("application/json")]
+    public class CategoryController : Controller
+    {
+        [Route("api/categories")]
+        public IEnumerable<Category> Get()
+        {
+            return BlogContextManager.Categories.OrderBy(c => c.Name);
+        }
+    }
+}
