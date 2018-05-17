726656c6e04f6f018ebbda48cdec518f680a6b34
michaeldeongreen <michaeldeongreen@gmail.com>
2018-05-16 19:43:35 -0500

    *Fixed issue with publish.sh

--- publish.sh
+++ publish.sh
@@ -4,6 +4,6 @@ rm -rf "c:/temp/My Blog Uploads/dist/api" && mkdir "c:/temp/My Blog Uploads/dist
 dotnet publish "C:\Git\Blog.GrenitausConsulting\Core.WebApi\src\Blog.GrenitausConsulting.Core.Web.Api" -c Release -o "C:\temp\My Blog Uploads\dist\api"
 
 cd c:/Git/Blog.GrenitausConsulting/Client
-ng build --output-path "C:\temp\My Blog Uploads\dist\client"
+ng build --prod --output-path "C:\temp\My Blog Uploads\dist\client"
 
 exit 0
