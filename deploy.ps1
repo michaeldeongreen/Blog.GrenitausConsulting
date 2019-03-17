ec27d3122c7b6a7bf6d392e2d0b658c1e1804436
michaeldeongreen <michaeldeongreen@gmail.com>
2019-03-17 07:23:58 -0500

    Implemented deploy.ps1

---
+++ deploy.ps1
@@ -0,0 +1,24 @@
+<#
+Powershell script is uses azure-cli to deploy the Blog Web API and Blog Web Client.
+Parameters are:
+1. $username: Service Principal Username
+2. $password: Service Principal Password
+3. $tenant: Tenant Id
+4. $resourcegroup: WebApp Resource Group
+5. $webapiname: WebApp Web API Name
+6. $webclientname: WebApp Web Client Name
+7. $workingdirectory: Release Pipeline Working Directory
+8. $builddefinitionname: Build Pipeline Definition Name
+#>
+param([string]$username,[string]$password,[string]$tenant,[string]$resourcegroup,[string]$webapiname,
+[string]$webclientname,[string]$workingdirectory,[string]$builddefinitionname)
+
+# Initialize variables
+$webapiZipFile = "$workdingdirectory\_$builddefinitionname\drop\webapi.zip"
+$webclientZipFile = "$workdingdirectory\_$builddefinitionname\drop\webclient.zip"
+
+# Login via Service Principal
+az login --service-principal --username $username --password $password --tenant $tenant
+
+# Deploy zipped files
+az webapp deployment source config-zip --name $webclientname --resource-group $resourcegroup --src $webclientZipFile
